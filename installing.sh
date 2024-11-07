58b08dc52f499608ddc310714904191440935502
mothukuru saikiran <obscuresai.com>
2024-11-07 16:28:25 +0530

    messgae

--- installing.sh
+++ installing.sh
@@ -14,10 +14,10 @@ N="\e[0m"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
-        echo -e "$2.......$R FAILED $N"
+        echo -e "$Y $2.......$R FAILED $N"
         exit 1
     else
-        echo -e "$2.......$G SUCCESS $N"
+        echo -e "$Y $2.......$G SUCCESS $N"
     fi
 }
 
