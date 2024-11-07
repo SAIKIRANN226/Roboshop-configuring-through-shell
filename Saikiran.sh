759e8b2c5ad802ceca1e374f358a97e058c681bb
mothukuru saikiran <obscuresai.com>
2024-11-07 16:01:45 +0530

    messgae

--- Saikiran.sh
+++ Saikiran.sh
@@ -1,42 +1,43 @@
 #!/bin/bash
 
 ID=$(id -u)
-DATE=$(date)
+DATE=$(date %A)
 
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGFILE="/tmp/$0-$DATE.log"
+LOGFILE="/tmp/$0/$DATE.log"
 
 VALIDATE() {
     if [ $1 -ne 0 ]
     then 
-        echo -e "$2.....$R FAILED $N"
+        echo -e "$2.......$R FAILED $N"
         exit 1
     else
-        echo -e "$2.....$G SUCCESS $N"
+        echo -e "$2.......$G SUCCESS $N"
     fi
 }
+
 
 if [ $ID -ne 0 ]
 then 
     echo -e "$R ERROR:: Please run the script with root user $N"
     exit 1
-else
+else    
     echo -e "$Y Script started executing at ${DATE} $N"
-fi
+fi 
 
 
-for package in $@
-do
-    yum list installed $package
-    if [ $? -ne 0 ]
-    then 
-        yum install $package -y $>> $LOGFILE
-        VALIDATE $1 "$package Installation"
-    else
-        echo -e "$package already installed so $Y ....SKIPPING $N"
-    fi
-done
+yum install mysql -y $>> $LOGFILE
+
+VALIDATE $? "Installing mysql"
+
+yum install git -y $>> $LOGFILE
+
+VALIDATE $? "Installing git"
+
+yum install postfix -y $>> $LOGFILE
+
+VALIDATE $? "Installing postfix"
