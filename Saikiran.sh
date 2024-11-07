dca3664a82ca672fe9c79f262565607242db8803
mothukuru saikiran <obscuresai.com>
2024-11-07 15:34:53 +0530

    messgae

---
+++ Saikiran.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+ID=$(id -u)
+DATE=$(date)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGFILE="/tmp/$0/saikiran.log"
+
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2.....$R FAILED $N"
+        exit 1
+    else
+        echo -e "$2.....$G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then 
+    echo -e "$R ERROR:: Please run the script with root user $N"
+    exit 1
+else
+    echo -e "$Y Script started executing at ${DATE} $N"
+fi
+
+
+for package in $@
+do
+    yum list installed $package
+    if [ $? -ne 0 ]
+    then 
+        yum install $package -y $>> $LOGFILE
+    else
+        echo -e "$package .....already installed so $Y SKIPPING $N"
+    fi
+done
