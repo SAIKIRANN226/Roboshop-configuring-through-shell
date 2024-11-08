498e29816ae0fb7ef16be5c3369f95d467df9784
mothukuru saikiran <obscuresai.com>
2024-11-08 13:48:03 +0530

    messgae

---
+++ catalogue.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$Y $2.......$R FAILED $N"
+        exit 1
+    else
+        echo -e "$Y $2.......$G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then 
+    echo -e "$R ERROR:: Please run the script with root user $N"
+    exit 1
+else
+    echo -e "Root user"
+fi
+
+dnf module disable nodejs -y
+
+VALIDATE $? "Disabling nodejs"
+
+dnf module enable nodejs:18 -y
+
+VALIDATE $? "Enabling nodejs:18"
+
+dnf install nodejs -y
+
+VALIDATE $? "Installing nodejs"
+
+useradd roboshop -y
+
+VALIDATE $? "Adding roboshop user"
+
+mkdir  /app -y
+
+VALIDATE $? "Creating app folder"
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip -y
+
+VALIDATE $? "Downloading the code"
+
+unzip /tmp/catalogue.zip -y
+
+VALIDATE $? "Unzipping the code"
+
+
