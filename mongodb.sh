687b52d90eda3f0854d62e9277f7072149668faa
mothukuru saikiran <obscuresai.com>
2024-11-08 13:20:37 +0530

    messgae

--- mongodb.sh
+++ mongodb.sh
@@ -10,10 +10,10 @@ N="\e[0"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
-        echo -e "$2.....$R FAILED $N"
+        echo -e "$2.....$R FAILED"
         exit 1
     else
-        echo -e "$2....$G SUCCESS $N"
+        echo -e "$2....$G SUCCESS"
     fi
 }
 
