fbb950d8f3bddf8e260b599d148252b334f62f07
mothukuru saikiran <obscuresai.com>
2024-11-08 12:38:24 +0530

    messgae

--- shell.sh
+++ shell.sh
@@ -1,42 +1,30 @@
 #!/bin/bash
 
 ID=$(id -u)
-DATE=$(date)
-
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-
-echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+DATE=$(date)
 
 VALIDATE() {
     if [ $1 -ne 0 ]
-    then 
-        echo -e "$Y $2.....$R FAILED $N"
+    then    
+        echo -e "$Y $2......$R FAILED $N"
         exit 1
-    else
-        echo -e "$Y $2.....$G SUCCESS $N"
+    else    
+        echo -e "$Y $2......$G SUCCESS $N"
     fi
 }
 
-if [ $ID -ne 0 ]
-then 
-    echo -e "$R ERROR:: Please run the script with root user $N"
-    exit 1
-else
-    echo -e "$Y This script started executing at ${DATE} $N"
-fi
-
-
 for package in $@
-do 
+do
     yum list installed $package
     if [ $? -ne 0 ]
-    then 
+    then    
         yum install $package -y
     else
-        echo -e "$package is already installed so $Y.....SKIPPING $N"
+        echo -e "$Y $package is already installed so $Y.......SKIPPING $N"
     fi
 done
