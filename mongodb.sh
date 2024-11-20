c77a6ad5cf40b49a7131b0b2ff7d8179b8d3c59a
mothukuru saikiran <obscuresai.com>
2024-11-20 15:39:02 +0530

    messgae

--- mongodb.sh
+++ mongodb.sh
@@ -6,24 +6,48 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-if [ $1 - ne 0 ]
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2.....$R FAILED $N"
+        exit 1
+    else
+        echo -e "$2.....$G SUCCESS $N"
+    fi
+}
+
+if [ $ID - ne 0 ]
 then 
-    echo -e "$2.....$R FAILED $N"
+    echo -e "$R ERROR:: Please run the script with root user $N"
     exit 1
 else
-    echo -e "$2.....$G SUCCESS $N"
-fi 
-
-if [ $ID -ne 0 ]
-then 
-    echo -e "$R ERROR: Please run the script with root user $N"
-    exit 1
-else    
     echo -e "$Y Root user $N"
 fi 
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 
-VALIDATE $? "copied mongo repo
+VALIDATE $? "Copied mongo.repo file"
 
+dnf install mongodb-org -y &>> $LOGFILE
+
+VALIDATE $? "Installing mongodb"
+
+systemctl enable mongod
+
+VALIDATE $? "Enabling mongodb"
+
+systemctl start mongodb
+
+VALIDATE $? "Starting mongodb"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
+
+VALIDATE $? "Given remote access"
+
+systemctl restart mongod
+
+VALIDATE $? "Restarted mongodb"
 
