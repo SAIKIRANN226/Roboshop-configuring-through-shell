ed018e5a9eeb5aedc817363c98ec3158fb7a12fd
mothukuru saikiran <obscuresai.com>
2024-12-11 12:30:59 +0530

    created repo for CD part

--- shell.sh
+++ shell.sh
@@ -17,7 +17,7 @@ VALIDATE() {
 if [ $ID -ne 0 ]
 then 
     echo -e "$R ERROR:: Please run the script with root user $N"
-    exit 1
+    #exit 1
 else
     echo -e "$G Now you are root user you can install anything $N"
 fi
@@ -28,7 +28,7 @@ yum install mysql -y
 if [ $? -ne 0 ]
 then 
     echo -e "$R ERROR:: Installing mysql is failed $N"
-    exit 1
+    #exit 1
 else
     echo -e "$G Installing mysql success $N"
 fi 
