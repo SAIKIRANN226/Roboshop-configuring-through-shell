138a9432e25188efb666e3e7f008f3d3551cef47
mothukuru saikiran <obscuresai.com>
2024-12-11 12:28:22 +0530

    created repo for CD part

--- shell.sh
+++ shell.sh
@@ -3,6 +3,17 @@
 ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
+
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$1......$R...FAILED $N"
+        exit 1
+    else
+        echo -e "$2......$G....SUCCESS $N"
+    fi 
+}
+
 if [ $ID -ne 0 ]
 then 
     echo -e "$R ERROR:: Please run the script with root user $N"
@@ -21,3 +32,11 @@ then
 else
     echo -e "$G Installing mysql success $N"
 fi 
+
+yum install postfix -y
+
+VALIDATE $? "Installing postfix"
+
+yum install git -y
+
+VALIDATE $? "Installing git"
