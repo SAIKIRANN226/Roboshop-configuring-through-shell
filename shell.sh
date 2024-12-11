922d474eb878abc3fac27b203e9f445011b222b5
mothukuru saikiran <obscuresai.com>
2024-12-11 12:18:23 +0530

    created repo for CD part

---
+++ shell.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+if [ $ID -ne 0 ]
+then 
+    echo -e "$R ERROR:: Please run the script with root user $N"
+    exit 1
+else
+    echo -e "$G Now you are root user you can install anything $N"
+fi
+
+
+yum install mysql 
+
+if [ $? -ne 0 ]
+then 
+    echo -e "$R ERROR:: Installing mysql is failed $N"
+    exit 1
+else
+    echo -e "$G Installing mysql success $N"
+fi 
