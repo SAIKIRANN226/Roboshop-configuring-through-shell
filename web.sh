afc84b935dd924c7b1ebee6aa678e766f25c013c
mothukuru saikiran <obscuresai.com>
2024-12-25 18:23:33 +0530

    message

---
+++ web.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+DATE=$(date)
+LOGFILE="/tmp/$0-$DATE.log"
+
+if [ $ID -ne 0 ]
+then 
+    echo -e "$R ERROR:: Please run the script with root user $N"
+    exit 1
+else
+    echo -e "$Y Script started executing at $DATE $N"
+fi 
+
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2....$R FAILED $N"
+        exit 1
+    else
+        echo -e "$2....$G SUCCESS $N"
+fi 
+}
+
+
+dnf install nginx -y &>> LOGFILE
+
+VALIDATE $? "Installing nginx"
+
+systemctl enable nginx &>> LOGFILE
+
+VALIDATE $? "Enabling nginx"
+
+systemctl start nginx &>> LOGFILE
+
+VALIDATE $? "Starting nginx"
+
+rm -rf /usr/share/nginx/html/* &>> LOGFILE
+
+VALIDATE $? "Removing default content"
+
+curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip &>> LOGFILE
+
+VALIDATE $? "Downloading the front end content"
+
+cd /usr/share/nginx/html &>> LOGFILE
+
+VALIDATE $? "Extracting the front end content"
+
+unzip /tmp/web.zip &>> LOGFILE
+
+VALIDATE $? "Extracting the front end content"
+
+systemctl restart nginx &>> LOGFILE
+
+VALIDATE $? "Restarting the nginx"
+
+
+
+
